80227b3cfaa46a6d148076fe7d70e7064be105f6
root <crazytheshow@gmail.com>
2018-03-15 17:34:32 -0400

    Actualización del Game y funcionalidad. Falta el empate y pedida de NickName's

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
 const CLIC1 = document.getElementsByClassName("row-item");
 
 let letra = "X";
-let letra1 = "0";
 
 for (let i = 0; i < CLIC1.length; i++) {
 	CLIC1[i].addEventListener('click', mostrar);
 	
 	function mostrar(e) {
-		e = CLIC1[i].innerHTML = letra;
-		
-		if (letra = 0) {
-			CLIC1[i].innerHTML = letra1;
+
+		if (e.target.innerHTML === "X" || e.target.innerHTML === "0") {
+			e.target === innerHTML;
+
+		}else if (letra === "X") {
+			CLIC1[i].innerHTML = "X";
+			letra = "0";
+		}else{
+			CLIC1[i].innerHTML = "0";
+			letra = "X";
 		}
 
-		console.log(e);
+			
+		if((CLIC1[0].innerHTML === CLIC1[1].innerHTML && CLIC1[1].innerHTML === CLIC1[2].innerHTML &&
+			CLIC1[0].innerHTML != "" && CLIC1[1].innerHTML != "" && CLIC1[2].innerHTML != "") ||
+				(CLIC1[3].innerHTML === CLIC1[4].innerHTML && CLIC1[4].innerHTML === CLIC1[5].innerHTML && 
+		 		 CLIC1[3].innerHTML != "" && CLIC1[4].innerHTML != "" && CLIC1[5].innerHTML != "") ||
+					(CLIC1[6].innerHTML === CLIC1[7].innerHTML && CLIC1[7].innerHTML === CLIC1[8].innerHTML && 
+				 	 CLIC1[6].innerHTML != "" && CLIC1[7].innerHTML != "" && CLIC1[8].innerHTML != "") ||
+		 		 		(CLIC1[2].innerHTML === CLIC1[4].innerHTML && CLIC1[4].innerHTML === CLIC1[6].innerHTML && 
+				 	 	 CLIC1[2].innerHTML != "" && CLIC1[4].innerHTML != "" && CLIC1[6].innerHTML != "") ||
+		 		 			(CLIC1[0].innerHTML === CLIC1[4].innerHTML && CLIC1[4].innerHTML === CLIC1[8].innerHTML && 
+				 	 	 	 CLIC1[0].innerHTML != "" && CLIC1[4].innerHTML != "" && CLIC1[8].innerHTML != "") ||
+		 		 				(CLIC1[2].innerHTML === CLIC1[5].innerHTML && CLIC1[5].innerHTML === CLIC1[8].innerHTML && 
+				 	 	 	 	 CLIC1[2].innerHTML != "" && CLIC1[5].innerHTML != "" && CLIC1[8].innerHTML != "") ||
+									(CLIC1[1].innerHTML === CLIC1[4].innerHTML && CLIC1[4].innerHTML === CLIC1[7].innerHTML && 
+					 	 	 	 	 CLIC1[1].innerHTML != "" && CLIC1[4].innerHTML != "" && CLIC1[7].innerHTML != "") ||
+										(CLIC1[0].innerHTML === CLIC1[3].innerHTML && CLIC1[3].innerHTML === CLIC1[6].innerHTML && 
+					 		 	 	 	 CLIC1[0].innerHTML != "" && CLIC1[3].innerHTML != "" && CLIC1[6].innerHTML != "")) {
+			setTimeout(function(){
+				alert("GANÓ LA " + e.target.innerHTML);
+				for (let i = 0; i < CLIC1.length; i++) {
+					CLIC1[i].innerHTML = "";
+				}
+			}, 1000);
 	}
-
+}
 }
